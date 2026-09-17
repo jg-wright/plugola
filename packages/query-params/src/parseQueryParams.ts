49f211869478fc3d22259ffce55f98b05a90af5f
John Wright <john.wright@mailmetromedia.co.uk>
2026-09-17 17:03:14 +0100

    refactor(query-params)!: simplify api
    BREAKING CHANGE: `amendKey` and `filter` have been removed in favour of `set`.

--- packages/query-params/src/parseQueryParams.ts
+++ packages/query-params/src/parseQueryParams.ts
@@ -2,35 +2,22 @@ import type { QueryParam, QueryParams } from './queryParams.js'
 
 interface Options {
   /**
-   * Change the key before it gets added to the resulting object
-   */
-  amendKey?(key: string): string
-  /**
    * Merge result in to an object
    */
   into?: QueryParams
-  /**
-   * Pick query parameters by key
-   */
-  filter?(key: string, value: string | undefined): boolean
   /**
    * Custom merging logic
    */
   set?(
     queryParams: QueryParams,
     key: string,
-    value: QueryParam | QueryParam[]
+    value: QueryParam | QueryParam[],
   ): QueryParams
 }
 
 export default function parseQueryParams(
   searchString: string,
-  {
-    amendKey = (k) => k,
-    into = {},
-    filter = () => true,
-    set = (qp, k, v) => ({ ...qp, [k]: v }),
-  }: Options = {}
+  { into = {}, set = (qp, k, v) => ({ ...qp, [k]: v }) }: Options = {},
 ) {
   searchString = searchString.replace(/^\?/, '')
   if (!searchString) return into
@@ -41,33 +28,29 @@ export default function parseQueryParams(
       .reduce<QueryParams>((queryParams, searchParam) => {
         let [key, value] = searchParam.split('=', 2) as [
           string,
-          string | undefined
+          string | undefined,
         ]
 
         key = decodeURIComponent(key)
 
-        if (!filter(key, value)) return queryParams
-
-        if (key.startsWith('!'))
-          return set(queryParams, amendKey(key.slice(1)), false)
-        else if (value === undefined)
-          return set(queryParams, amendKey(key), true)
+        if (key.startsWith('!')) return set(queryParams, key.slice(1), false)
+        else if (value === undefined) return set(queryParams, key, true)
         else if (key.endsWith('[]'))
-          return set(queryParams, amendKey(withoutEnd(key, 2)), toArray(value))
+          return set(queryParams, withoutEnd(key, 2), toArray(value))
         else if (key.endsWith('[^]')) {
-          key = amendKey(withoutEnd(key, 3))
+          key = withoutEnd(key, 3)
           return set(queryParams, key, prependArray(value, queryParams[key]))
         } else if (key.endsWith('[+]')) {
-          key = amendKey(withoutEnd(key, 3))
+          key = withoutEnd(key, 3)
           return set(queryParams, key, appendArray(value, queryParams[key]))
         } else if (key.endsWith('[-]')) {
-          key = amendKey(withoutEnd(key, 3))
+          key = withoutEnd(key, 3)
           return set(queryParams, key, removeFromArray(value, queryParams[key]))
         } else if (key.endsWith('{x}'))
-          return set(queryParams, amendKey(withoutEnd(key, 3)), toFlags(value))
+          return set(queryParams, withoutEnd(key, 3), toFlags(value))
         else if (key.endsWith('{}'))
-          return set(queryParams, amendKey(withoutEnd(key, 2)), fromJSON(value))
-        else return set(queryParams, amendKey(key), decodeURIComponent(value))
+          return set(queryParams, withoutEnd(key, 2), fromJSON(value))
+        else return set(queryParams, key, decodeURIComponent(value))
       }, into)
   } catch (error) {
     console.error('ads', error)
@@ -104,7 +87,7 @@ function removeFromArray(value: string, queryParam: unknown) {
           ? queryParam
           : [...queryParam.slice(0, index), ...queryParam.slice(index + 1)]
       },
-      Array.isArray(queryParam) ? queryParam : []
+      Array.isArray(queryParam) ? queryParam : [],
     )
 }
 
